c915df6114d7dd0f8cca7ca71b58a029b26daddd
companyMac <18630149092@139.com>
2016-10-21 13:48:20 +0800

    test

---
+++ src/test/java/com/peierlong/Greeter.java
@@ -0,0 +1,15 @@
+package com.peierlong;
+
+/**
+ * 包名: com.peierlong
+ * 创建人 : Elong
+ * 时间: 10/21/16 10:29 AM
+ * 描述 :
+ */
+public class Greeter {
+
+    public String sayHello(){
+        return "hello spring";
+    }
+
+}
