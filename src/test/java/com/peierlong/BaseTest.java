0df68c845b9c9bacb84f8a8167d5217275e81195
companyMac <18630149092@139.com>
2016-11-05 17:08:09 +0800

    spring mvc 初始导入...

---
+++ src/test/java/com/peierlong/BaseTest.java
@@ -0,0 +1,17 @@
+package com.peierlong;
+
+import org.junit.Test;
+
+/**
+ * 包名: com.peierlong
+ * 创建人 : Elong
+ * 时间: 11/5/16 2:08 PM
+ * 描述 : BaseTest
+ */
+public class BaseTest {
+
+    @Test
+    public void test(){
+        System.out.println("hello weSub!");
+    }
+}
